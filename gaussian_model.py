3eb31fd353c4687f1cd911e48d47f2caffbaf425
ZhengLong3 <ongzhenglong@gmail.com>
2024-09-28 19:07:40 +0800

    implement k_means on xyz

---
+++ gaussian_model.py
@@ -0,0 +1,102 @@
+import torch
+import numpy as np
+from torch import nn
+import os
+from plyfile import PlyData, PlyElement
+
+DEVICE = "cpu"
+
+# extracted from https://github.com/graphdeco-inria/gaussian-splatting/blob/main/scene/gaussian_model.py
+class GaussianModel:
+
+    def __init__(self, sh_degree : int):
+        self.active_sh_degree = 0
+        self.max_sh_degree = sh_degree  
+        self._xyz = torch.empty(0)
+        self._features_dc = torch.empty(0)
+        self._features_rest = torch.empty(0)
+        self._scaling = torch.empty(0)
+        self._rotation = torch.empty(0)
+        self._opacity = torch.empty(0)
+        self.max_radii2D = torch.empty(0)
+        self.xyz_gradient_accum = torch.empty(0)
+        self.denom = torch.empty(0)
+        self.optimizer = None
+        self.percent_dense = 0
+        self.spatial_lr_scale = 0
+
+    def construct_list_of_attributes(self):
+        l = ['x', 'y', 'z', 'nx', 'ny', 'nz']
+        # All channels except the 3 DC
+        for i in range(self._features_dc.shape[1]*self._features_dc.shape[2]):
+            l.append('f_dc_{}'.format(i))
+        for i in range(self._features_rest.shape[1]*self._features_rest.shape[2]):
+            l.append('f_rest_{}'.format(i))
+        l.append('opacity')
+        for i in range(self._scaling.shape[1]):
+            l.append('scale_{}'.format(i))
+        for i in range(self._rotation.shape[1]):
+            l.append('rot_{}'.format(i))
+        return l
+
+    def save_ply(self, path):
+        os.makedirs(os.path.dirname(path), exist_ok=True)
+
+        xyz = self._xyz.detach().cpu().numpy()
+        normals = np.zeros_like(xyz)
+        f_dc = self._features_dc.detach().transpose(1, 2).flatten(start_dim=1).contiguous().cpu().numpy()
+        f_rest = self._features_rest.detach().transpose(1, 2).flatten(start_dim=1).contiguous().cpu().numpy()
+        opacities = self._opacity.detach().cpu().numpy()
+        scale = self._scaling.detach().cpu().numpy()
+        rotation = self._rotation.detach().cpu().numpy()
+
+        dtype_full = [(attribute, 'f4') for attribute in self.construct_list_of_attributes()]
+
+        elements = np.empty(xyz.shape[0], dtype=dtype_full)
+        attributes = np.concatenate((xyz, normals, f_dc, f_rest, opacities, scale, rotation), axis=1)
+        elements[:] = list(map(tuple, attributes))
+        el = PlyElement.describe(elements, 'vertex')
+        PlyData([el]).write(path)
+
+    def load_ply(self, path):
+        plydata = PlyData.read(path)
+
+        xyz = np.stack((np.asarray(plydata.elements[0]["x"]),
+                        np.asarray(plydata.elements[0]["y"]),
+                        np.asarray(plydata.elements[0]["z"])),  axis=1)
+        opacities = np.asarray(plydata.elements[0]["opacity"])[..., np.newaxis]
+
+        features_dc = np.zeros((xyz.shape[0], 3, 1))
+        features_dc[:, 0, 0] = np.asarray(plydata.elements[0]["f_dc_0"])
+        features_dc[:, 1, 0] = np.asarray(plydata.elements[0]["f_dc_1"])
+        features_dc[:, 2, 0] = np.asarray(plydata.elements[0]["f_dc_2"])
+
+        extra_f_names = [p.name for p in plydata.elements[0].properties if p.name.startswith("f_rest_")]
+        extra_f_names = sorted(extra_f_names, key = lambda x: int(x.split('_')[-1]))
+        assert len(extra_f_names)==3*(self.max_sh_degree + 1) ** 2 - 3
+        features_extra = np.zeros((xyz.shape[0], len(extra_f_names)))
+        for idx, attr_name in enumerate(extra_f_names):
+            features_extra[:, idx] = np.asarray(plydata.elements[0][attr_name])
+        # Reshape (P,F*SH_coeffs) to (P, F, SH_coeffs except DC)
+        features_extra = features_extra.reshape((features_extra.shape[0], 3, (self.max_sh_degree + 1) ** 2 - 1))
+
+        scale_names = [p.name for p in plydata.elements[0].properties if p.name.startswith("scale_")]
+        scale_names = sorted(scale_names, key = lambda x: int(x.split('_')[-1]))
+        scales = np.zeros((xyz.shape[0], len(scale_names)))
+        for idx, attr_name in enumerate(scale_names):
+            scales[:, idx] = np.asarray(plydata.elements[0][attr_name])
+
+        rot_names = [p.name for p in plydata.elements[0].properties if p.name.startswith("rot")]
+        rot_names = sorted(rot_names, key = lambda x: int(x.split('_')[-1]))
+        rots = np.zeros((xyz.shape[0], len(rot_names)))
+        for idx, attr_name in enumerate(rot_names):
+            rots[:, idx] = np.asarray(plydata.elements[0][attr_name])
+
+        self._xyz = torch.tensor(xyz, dtype=torch.float, device=DEVICE)
+        self._features_dc = torch.tensor(features_dc, dtype=torch.float, device=DEVICE).transpose(1, 2).contiguous()
+        self._features_rest = torch.tensor(features_extra, dtype=torch.float, device=DEVICE).transpose(1, 2).contiguous()
+        self._opacity = torch.tensor(opacities, dtype=torch.float, device=DEVICE)
+        self._scaling = torch.tensor(scales, dtype=torch.float, device=DEVICE)
+        self._rotation = torch.tensor(rots, dtype=torch.float, device=DEVICE)
+
+        self.active_sh_degree = self.max_sh_degree
--- k_means.py
+++ k_means.py
@@ -1,6 +1,45 @@
-from plyfile import PlyData, PlyElement
-from gaussian-splatting.scene.gaussian_model import GaussianModel
+import torch
 
-with open("2_plane_3_edited.ply", "rb") as f:
-    ply_data = PlyData.read(f)
-    print(type(ply_data.elements[0].data[0]))
+from gaussian_model import GaussianModel
+
+NUM_CLUSTERS = 2
+MAX_ITER = 1000
+
+gaussian = GaussianModel(3)
+gaussian.load_ply("./2_plane_3_edited.ply")
+
+features = gaussian._xyz
+
+# randomly choose centers from input
+perm = torch.randperm(features.shape[0])
+centroids = features[perm[:NUM_CLUSTERS]]
+
+labels = torch.zeros(features.shape[0], dtype=torch.uint8)
+repeated_features = features.unsqueeze(1).repeat(1, NUM_CLUSTERS, 1)
+
+# k-means
+for _ in range(MAX_ITER):
+    old_centroids = centroids
+
+    # assign closest
+    difference = repeated_features - centroids
+    norm = torch.linalg.norm(difference, ord=2, dim=-1)
+    labels = torch.argmin(norm, dim=1)
+
+    # get new centroids
+    # taken from https://stackoverflow.com/a/56155805
+    M = torch.zeros(labels.max() + 1, len(features))
+    M[labels, torch.arange(len(features))] = 1
+    M = torch.nn.functional.normalize(M, p=1, dim=1)
+    centroids = torch.mm(M, features)
+
+    if torch.mean(torch.linalg.norm(centroids - old_centroids, ord=2, dim=1)) < 0.00001:
+        break
+
+new_features_dc = labels.unsqueeze(-1).unsqueeze(-1).repeat(1, 1, 3)
+gaussian._features_dc = new_features_dc
+
+new_features_rest = labels.unsqueeze(-1).unsqueeze(-1).repeat(1, 15, 3)
+gaussian._features_rest = new_features_rest
+
+gaussian.save_ply("./2_plane_3_clustered.ply")
